db48d0ffe5565a082fc06f17b9042040b83f8a19
benjamin <benjamin.marissiaux@student.hepl.be>
2024-01-13 13:35:18 +0100

    examen-janvier-2019-2020

--- main.js
+++ main.js
@@ -1,42 +1,79 @@
 // Nom : 
 // Prénom : 
 // Groupe :
-
-
 document.querySelector('html').classList.add('js-enabled');
 
-
-for (let i = 199; i > 0; i = i - 2) {
-    document.querySelector('#app').insertAdjacentHTML('afterbegin', `<li class="grid__item">${i}</li>`);
-
-    document.querySelectorAll('li').forEach((liElement) => {
-        if (i % 9 === 0 && i % 3 === 0) {
-            liElement.classList.add('multiple-3-9');
-            liElement.insertAdjacentHTML('afterbegin', `<div class="ribbon-wrapper">
-    <div class="ribbon">3 et 9</div>
-  </div>`);
+for (let i = 1; i < 200; i++) {
+    if (i % 2) {
+        document.querySelector('#app').insertAdjacentHTML('beforeend', `<li data-text="somme" class="grid__item">${i}</li>`);
+    }
+}
+function isPrime(number) {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
         }
-        if (i % 3 === 0) {
-            liElement.classList.replace('multiple-3-9','multiple-3');
-            liElement.insertAdjacentHTML('afterbegin', `<div class="ribbon-wrapper">
+    }
+    return true;
+}
+isPrime();
+
+document.querySelectorAll('li').forEach(liElement => {
+    const texteToNumber = parseInt(liElement.textContent);
+    if (texteToNumber % 3 === 0 && texteToNumber % 9 === 0) {
+        liElement.classList.add('multiple-3-9');
+        liElement.insertAdjacentHTML('beforeend', `<div class="ribbon-wrapper">
+<div class="ribbon">3 et 9</div>`);
+    } else if (texteToNumber % 3 === 0) {
+        liElement.classList.add('multiple-3');
+        liElement.insertAdjacentHTML('beforeend', `<div class="ribbon-wrapper">
     <div class="ribbon">3</div>
   </div>`);
-        } else  {
-            liElement.classList.replace('mutliple-3','premier');
-            liElement.insertAdjacentHTML('afterbegin', `<div class="ribbon-wrapper">
-    <div data-text="somme" class="ribbon">premier</div>
-  </div>`);
-            liElement.addEventListener('click', ()=>{
-                liElement.classList.toggle('animate');
-            });
-        }
+    } else if (isPrime(texteToNumber) && texteToNumber > 1) {
+        liElement.classList.add('premier');
+        liElement.insertAdjacentHTML('beforeend', `<div class="ribbon-wrapper">
+    <div data-text="somme" class="ribbon">premier</div>`);
+    }
+});
+
+document.querySelectorAll('.premier').forEach(premier => {
+    premier.addEventListener('click', event => {
+        event.currentTarget.classList.add('animate');
+        document.querySelectorAll('li').forEach(li => {
+            li.classList.add('grid__item--lighter');
+        });
+
     });
-}
+    premier.addEventListener('transitionend', event => {
+        event.currentTarget.classList.remove('animate');
+        document.querySelector('ol').classList.remove('grid__item--lighter');
+        document.querySelectorAll('li').forEach(li => {
+            li.classList.remove('grid__item--lighter');
+        });
+    });
+});
 
 
-/* document.querySelectorAll('li').forEach(liElement => {
-     liElement.classList.add('multiple-3');
- })*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
